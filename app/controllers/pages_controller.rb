cf5ab2f1727d42ec46c9c24c16c1fd22a7b835a1
Inou Ridder <inouridder@gmail.com>
2017-04-24 16:18:27 +0200

    add show page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :gallery, :about, :contact ]
 
+  def test
+    @film = Film.all.first
+  end
+
   def home
   end
 
