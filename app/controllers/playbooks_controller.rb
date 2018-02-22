736976ab4ff42021065fd11633e6f9b46227c10c
Inou Ridder <inouridder@gmail.com>
2018-02-22 19:42:13 +0800

    add methods for colorization

--- app/controllers/playbooks_controller.rb
+++ app/controllers/playbooks_controller.rb
@@ -1,9 +1,11 @@
 class PlaybooksController < ApplicationController
   before_action :set_playbook
+  before_action :set_counts
   layout 'client'
 
   def show
     @times = Playline::TIMES
+
     @playline = Playline.new
     redirect_to root_path unless current_user.playbook == @playbook || current_user.admin
   end
@@ -19,6 +21,14 @@ class PlaybooksController < ApplicationController
   def set_playbook
     @playbook = Playbook.find(params[:id])
   end
+
+  def set_counts
+    p_count = @playbook.playlines.count
+    @count = p_count > 0 ? p_count : ""
+    m_count = @playbook.song_choices.count
+    @music_count = m_count > 0 ? m_count : ""
+  end
+
 end
 
 
--- app/models/playbook.rb
+++ app/models/playbook.rb
@@ -2,4 +2,26 @@ class Playbook < ApplicationRecord
   belongs_to :booking
   has_many :playlines
   has_many :song_choices
+
+  def colored_playline_state
+    count = self.playlines.count
+    if count == 0
+      "red"
+    elsif count > 10
+      "green"
+    else
+      "orange"
+    end
+  end
+
+  def colored_music_state
+    count = self.song_choices.count
+    if count == 0
+      "red"
+    elsif count >= 3
+      "green"
+    else
+      "orange"
+    end
+  end
 end
