72aa87cf8d41bbbe26893166dd552776682127d9
Inou Ridder <inouridder@gmail.com>
2018-03-24 12:26:28 +0800

    leave message on client film show

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
 
   get '/admin', to: 'admin/requests#index'
 
-  # please tell me I can delete this horrible route.
-  get 'playlines/:id', to: 'playlines#update'
-
   namespace :admin do
     resources :bookings do
       collection do
@@ -38,10 +35,10 @@ Rails.application.routes.draw do
         get 'end'
       end
     end
-    resources :films do
+  end
+    resources :films, only: [] do
       resources :comments, only: [:create]
     end
-  end
 
   resources :requests, only: [:create]
 
