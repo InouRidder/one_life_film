dc222cdae9a42f188034afa95c1729c4a00890bd
Inou Ridder <inouridder@gmail.com>
2018-03-29 21:02:56 +0800

    add component tables to admin scss for booking show

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -16,12 +16,11 @@ class Booking < ApplicationRecord
   has_many :songs, through: :song_choices
   pg_search_scope :search_by_text, :against => [:name, :location_wedding, :email_address]
 
-  # validates :phone_number, presence: true, format: { with: /\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/, message: "please enter a valid phonenumber" }
-  # validates :name, presence: true
-  # validates :email_address, presence: true
-  # validates :location_wedding, presence: true
-  # validates :date_wedding, presence: true, inclusion: { in: [Date.today..(Date.today + 730)] }
-  # validates :description, presence: true
+  validates :name, presence: true
+  validates :email_address, presence: true
+  validates :location_wedding, presence: true
+  validates :date_wedding, presence: true
+
   after_create :booking_services
 
   scope :active, -> { where("state != 'declined' AND date_wedding >= ? ", Date.today)}
