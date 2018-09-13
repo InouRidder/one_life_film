8e3308ce9c6bead8127457bb133b5ecbc28a4b53
Inou Ridder <inouridder@gmail.com>
2018-09-13 13:06:01 +0200

    fix order bug

--- app/models/film.rb
+++ app/models/film.rb
@@ -9,7 +9,7 @@ class Film < ApplicationRecord
   validates :video_url, presence: true
   validates :name, presence: true
 
-  scope :promos, -> {where(promo: true).first(10)}
+  scope :promos, -> {where(promo: true)}
 
   paginates_per 10
 
